631d6f58e4834a0e8c7879321660853f2f8625a3
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-04-09 23:39:41 +0530

    create main.js file and upload me2.jpg

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+//select DOM Items
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.qyerySelectorAll('.nav-items');
+
+//set initial state of menu
